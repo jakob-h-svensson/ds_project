b2f3afab2595e47844bf8284ae39c67ac41d9343
jakob-h-svensson <jakob.svensson88@gmail.com>
2026-01-29 15:22:43 +0100

    updates

--- data_page.py
+++ data_page.py
@@ -60,7 +60,21 @@ st.markdown("""
 **Projektetnamn**  \n Ekonomiresan  \n
 **Syfte**  \n Att undersöka hur börsen reagerar på stora geopolitiska och makroekonomiska händelser. Samt att visualisera detta i en interaktiv tidsresa.  \n
 **Metodik**  \n Jämförelse av två urvalsindex som jag själv har skapat, ett svenskt och ett amerikanskt, över perioden 2015-2025. Datan hämtas via yfinance, hanteras och bearbetas med hjälp av Pandas och lagras i en SQLite databas. Därefter sker visualiseringar med hjälp av Plotly och allt paketeras i en Streamlit applikation.  \n
-**Struktur**  \n *streamlit_app.py* - Huvudfil för applikationen. Här styrs applikationens struktur och flöde.  \n *utils.py* - Här samlas den huvudsakliga funktionaliteten för hämtning och bearbetning av data.  \n *info_page.py* - Används för att presentera information om projektet och ge användaren en överblick.  \n *visualisation_page.py* - Filen för att visualisera data.  \n *data_page.py* - Denna fil presenterar projektet och dess uppbyggnad i en Streamlit-sida.  \n *data* - Denna mapp innehåller events.csv som används för presentation av händelser.  \n *data/database* - Denna mapp innehåller tickers.sqlite, databasen där yfinance-datan sparas.  \n
+**Struktur** \n
+`streamlit_app.py` - Huvudfil för applikationen. Här styrs applikationens struktur och flöde.
+
+`utils.py` - Här samlas den huvudsakliga funktionaliteten för hämtning och bearbetning av data.
+
+`info_page.py` - Används för att presentera information om projektet och ge användaren en överblick.
+
+`visualisation_page.py` - Används för presentation av visualiseringar och interaktion med data.
+
+`data_page.py` - Används för presentation av data och dess uppbyggnad.
+
+`data` - Innehåller events.csv som används för presentation av händelser.
+
+`data/database` - Innehåller tickers.sqlite, databasen där yfinance-datan sparas.
+
 **Konfiguration**  \n Se *requirements.txt* för applikationens krav och beroenden. Rekommenderade visnings-settings i Streamlit är "Light" \n
 """)
 
